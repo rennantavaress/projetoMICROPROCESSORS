0151216278ebb5aea00ed30d6d6e5de0fef3b65e
Rennan <rennantavares223@gmail.com>
2026-05-17 11:16:24 -0300

    Adicionando Interface em react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import DashboardTema2 from './Dashboard'
 
 import './App.css'
 
 export default function App() {
   return (
     <>
-      
+      <DashboardTema2 />
     </>
   )
 }
